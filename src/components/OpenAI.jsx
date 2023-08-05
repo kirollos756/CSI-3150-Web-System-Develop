42069f120241e73b7b279499e5805d7034d16756
AmelioMansour <ameliomansour@oakland.edu>
2023-08-05 16:42:39 -0400

    added loading icon for openAI component, need to make appear/disappear when needed

--- src/components/OpenAI.jsx
+++ src/components/OpenAI.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { State } from '@splidejs/splide';
+import chefloading from '../images/chef.png';
 
 
 
@@ -18,6 +19,8 @@ const OpenAIComponent = () => {
     };
 
     const handleSubmit = async (e) => {
+        //enable view of loading icon here
+
         e.preventDefault();
 
         const prompt = `Please provide a recipe using the following ingredients only: ${ingredients} 
@@ -62,7 +65,7 @@ const OpenAIComponent = () => {
                         {
                             role: 'system', content: `You are chefBot you simply do directly as you are told and do not add
                         any notes or extra messages. Also, do not keep any recollection of previous requests made to you. Treat each request as if its the first one received.` },
-                        { role: 'system', content: prompt}
+                        { role: 'system', content: prompt }
                     ],
                 },
                 {
@@ -79,10 +82,10 @@ const OpenAIComponent = () => {
             console.log(messageContent);
             //sets "Instructions" state to the response from Open AI API
             setInstructions(messageContent);
-            
+
             const recipesData = parseInstructions(messageContent);
 
-            
+
             setRecipes(recipesData);
         } catch (error) {
             console.error('Error:', error);
@@ -90,44 +93,44 @@ const OpenAIComponent = () => {
     };
 
     const parseInstructions = (instructions) => {
-      
+
 
 
         const recipeRegex = /Recipe: (.*?)\n\nIngredients:\n([\s\S]+)\n\nInstructions:\n([\s\S]+)/;
         const stepRegex = /\d+\.\s(.+)/g;
-    
+
         const recipesData = [];
-        
+
         const recipeMatch = recipeRegex.exec(instructions);
         if (!recipeMatch) {
             console.error("Parsing error: Could not extract recipe data.");
             return recipesData;
         }
-    
+
         const mealTitle = recipeMatch[1].trim();
         setRecipeTitle(mealTitle);
         const ingredients = recipeMatch[2].trim();
         const instructionsText = recipeMatch[3].trim();
-    
-    
+
+
         const stepsData = [];
         let stepMatch;
         while ((stepMatch = stepRegex.exec(instructionsText)) !== null) {
             const stepInstruction = stepMatch[1].trim();
             stepsData.push(stepInstruction);
         }
-    
+
         recipesData.push({
             meal_title: mealTitle,
             ingredients: ingredients.split('\n').map(item => item.trim()),
             instructions: stepsData,
         });
-    
-       
+
+
         return recipesData;
     };
-    
-    
+
+
 
 
 
@@ -140,13 +143,33 @@ const OpenAIComponent = () => {
         const link = document.createElement("a");
         link.href = URL.createObjectURL(blob);
         //Potentially should improve date format
-        link.download = "recipe-"+recipeTitle+"-" + Date.now() + ".txt";
+        link.download = "recipe-" + recipeTitle + "-" + Date.now() + ".txt";
         document.body.appendChild(link);
         link.click();
+    };
+    
+    
+    //Styling for Loading icon
+    const loadingIconStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '10vh',
+        animation: 'spin 2s linear infinite', // Adjust the duration and animation timing as needed
       };
-
+    
+      // CSS keyframes for the spinning animation
+      const keyframes = `@keyframes spin {
+        from {
+          transform: rotate(0deg);
+        }
+        to {
+          transform: rotate(360deg);
+        }
+      }`;
     return (
         <div>
+             <style>{keyframes}</style>
             <form onSubmit={handleSubmit}>
                 <p>Please enter ingredients seperated by commas</p>
                 <label htmlFor="ingredients">Enter Ingredients:</label>
@@ -169,13 +192,15 @@ const OpenAIComponent = () => {
                     </ul>
                     <h4>Instructions:</h4>
                     {recipe.instructions.map((instruction, i) => (
-                        <p key={i}>{i+1}.{instruction}</p>
+                        <p key={i}>{i + 1}.{instruction}</p>
                     ))}
-                
-                </div>
-                
-            ))}
 
+                </div>
+
+            ))}
+            <div class="loading-icon" style={loadingIconStyle}>
+                <img src={chefloading} alt="chef loading icon" />
+            </div>
             <div>
                 {/* Button for saving to text */}
                 {/*Updated function to use the instructionstate */}
